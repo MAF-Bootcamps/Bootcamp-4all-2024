91b5685cebe4e874c592a8b280155360ae041c47
Alan Santos <Alan Ferreira dos Santos>
2024-08-28 21:06:09 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,35 @@
+# Programação Web - Bootcamp 4all 2024
+
+Repositório criado para documentar os exemplos e exercícios durante as aulas
+
+## HTML
+
+- [Slides](https://slides.com/alanfsantos/html5)
+- [Documentação Oficial](https://developer.mozilla.org/pt-BR/docs/Web/HTML)
+- [Acessibilidade](https://developer.mozilla.org/pt-BR/docs/Learn/Accessibility/HTML)
+- [Validação de Formulários](https://developer.mozilla.org/en-US/docs/Learn/Forms/Form_validation)
+## CSS
+
+- [Slides](https://slides.com/alanfsantos/css)
+- [Guia de Unidades de Medidas](https://www.alura.com.br/artigos/guia-de-unidades-no-css)
+- [Documentação Oficial](https://developer.mozilla.org/pt-BR/docs/Web/CSS)
+- [Pseudo-classes](https://developer.mozilla.org/en-US/docs/Web/CSS/Pseudo-classes)
+- [Guia do Flexbox](https://origamid.com/projetos/flexbox-guia-completo/)
+- [Flexbox Froggy](https://codepip.com/games/flexbox-froggy/)
+- [Guia do CSS Grid](https://www.origamid.com/projetos/css-grid-layout-guia-completo/)
+- [Grid Garden](https://codepip.com/games/grid-garden/)
+
+## JavaScript
+
+- [Slides](https://slides.com/alanfsantos/javascript)
+- [Documentação Oficial](https://developer.mozilla.org/pt-BR/docs/Web/JavaScript)
+- [API Document](https://developer.mozilla.org/pt-BR/docs/Web/API/Window/document)
+- [querySelector](https://developer.mozilla.org/pt-BR/docs/Web/API/Document/querySelector)
+- [querySelectorAll](https://developer.mozilla.org/pt-BR/docs/Web/API/Document/querySelectorAll)
+- [Manipulando o DOM](https://developer.mozilla.org/pt-BR/docs/Web/API/Document_Object_Model/Introduction)
+- [Eventos](https://developer.mozilla.org/en-US/docs/Web/Events)
+- [Classes Dinâmicas](https://developer.mozilla.org/pt-BR/docs/Web/API/Element/classList)
+- [Funções](https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Guide/Functions)
+- [Quando algo der errado](https://developer.mozilla.org/pt-BR/docs/Learn/JavaScript/First_steps/What_went_wrong)
+- [Utilizando o console.log](https://brayanarrieta.hashnode.dev/how-use-consolelog-like-a-pro)
+- 
---
+++ html/js/externo.js
@@ -0,0 +1,77 @@
+//alert("Olá vindo de js externo");
+
+// buscando um único elemento do HTML (DOM)
+let container = document.querySelector("#container");
+
+// buscando vários elementos do HTML (DOM);
+let containers = document.querySelectorAll(".container");
+
+// exibindo uma mensagem no console do navegador
+console.log(container, containers);
+
+// alterado o conteúdo de um elemento
+container.innerHTML = "Elemento 1 - Texto <strong>atualizado</strong>";
+
+// alterando propriedades do estilo do elemento
+container.style.border = "2px #000 solid";
+container.style.padding = "10px";
+// container.style.display = "none";
+
+// alterando a cor do segundo elemento da lista de elementos com a classe .container
+containers[1].style.backgroundColor = "blue";
+
+let botao = document.querySelector(".btn");
+
+// criando atributos
+console.log("desabilitando o botão");
+botao.setAttribute("disabled", "disabled");
+botao.setAttribute("type", "button");
+
+// removendo atributos
+console.log("habilitando o botão");
+botao.removeAttribute("disabled");
+
+// exibindo a lista de classes do elemento
+console.log(botao.classList);
+
+// adicionando uma classe ao elemento
+botao.classList.add("btn-arredondado");
+
+// removendo uma classe do elemento
+botao.classList.remove("btn-erro");
+
+// saber se um elemento possui uma classe específica
+console.log("Contém a classe btn-erro?", botao.classList.contains("btn-erro"));
+console.log("Contém a classe btn?", botao.classList.contains("btn"));
+
+botao.onclick = function () {
+  // atualizando o conteúdo do terceiro quadrado utilizando a funcão
+  carregarConteudo(containers[2], "Trocando o conteúdo do quadrado");
+};
+
+let botaoSenha = document.querySelector("#mostrarSenha");
+
+botaoSenha.onclick = function () {
+  let inputSenha = document.querySelector("#senha");
+  inputSenha.setAttribute("type", "text");
+};
+
+let inputSenha = document.querySelector("#senha");
+
+inputSenha.onblur = function () {
+  console.log(inputSenha.value);
+};
+
+// declaração de uma função
+function carregarConteudo(elemento, html) {
+  console.log("executando uma função");
+  //   document.querySelector("#container").innerHTML = "Novo conteúdo";
+  elemento.innerHTML = html;
+}
+
+// chamando a funcão criada acima
+// é possível informar parametros que serão utilizandos pela funcão.
+// atualizando o conteúdo do primeiro quadrado utilizando a funcão
+carregarConteudo(container, "Texto vindo de uma função");
+// atualizando o conteúdo do segundo quadrado utilizando a funcão
+carregarConteudo(containers[1], "Texto vindo de uma função 2");
